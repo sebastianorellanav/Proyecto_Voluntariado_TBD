ffdd5c47365051228e9a87fe24f3e2faa706ed7e
sebastian orellana v <sebastian.orellana.v@usach.cl>
2020-05-25 13:14:42 -0400

    se modifica la navbar

--- Frontend/src/components/navBar.vue
+++ Frontend/src/components/navBar.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="myNavBar">
-    <b-button v-b-toggle.sidebar-variant>Menu</b-button>
-    <b-sidebar id="sidebar-variant" title="Menu" bg-variant="dark" text-variant="light" shadow>
-      <div class="px-3 py-2">
-        <nav class="mb-3">
-            <b-nav vertical>
-                <b-nav-item href="/#" @click="hide">Home</b-nav-item>
-                <b-nav-item href="#link-2" @click="hide">Emergencias</b-nav-item>
-                <b-nav-item href="#link-2" @click="hide">Tareas</b-nav-item>
-                <b-nav-item-dropdown
-                    id="my-nav-dropdown"
-                    text="Voluntarios"
-                    toggle-class="nav-link-custom"
-                    >
-                    <b-dropdown-item>Segun Habilidad</b-dropdown-item>
-                    <b-dropdown-item>Segun Emergencia</b-dropdown-item>
-                    <b-dropdown-divider></b-dropdown-divider>
-                    <b-dropdown-item>Three</b-dropdown-item>
-                </b-nav-item-dropdown>
-                <b-nav-item href="/about" @click="hide">About us</b-nav-item>
+  <div class="myNavBar" >
+    <b-navbar type="dark" variant="dark" style="margin-bottom: 70px;">
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
 
-            </b-nav>
-          </nav>
-      </div>
-    </b-sidebar>
+      <!-- Navbar dropdowns -->
+      <b-nav-item-dropdown text="Emergencias" right>
+        <b-dropdown-item href="#">Todas las Emergencias</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown text="Tareas" right>
+        <b-dropdown-item href="#">Todas las Tareas</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      <b-nav-item-dropdown text="Voluntarios" right>
+        <b-dropdown-item href="#">Todos los Voluntarios</b-dropdown-item>
+        <b-dropdown-item href="#">Segun Habilidad</b-dropdown-item>
+        <b-dropdown-item href="#">Segun Emergencia</b-dropdown-item>
+      </b-nav-item-dropdown>
+
+      
+    </b-navbar-nav>
+  </b-navbar>
   </div>
 </template>
