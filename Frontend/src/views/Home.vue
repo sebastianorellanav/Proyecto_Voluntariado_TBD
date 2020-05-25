4221db4b103efa4f03b5fe4a90c60889dd401ad7
sebastian orellana v <sebastian.orellana.v@usach.cl>
2020-05-25 11:39:02 -0400

    creación proyecto vue

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Bienvenido a Voluntariadapp"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
